2daa1f63942b825e2e982b4c730eba8f9c6759b2
Marko Zecevic <marczecevic@gmail.com>
2021-04-05 11:54:35 +0200

    Select in select

--- answers.sql
+++ answers.sql
@@ -66,3 +66,83 @@ AND name LIKE '%o%'
 AND name LIKE '%u%'
 AND name NOT LIKE '% %'
 
+-- SELECT FROM NOBEL
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+ SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+SELECT yr,subject
+FROM nobel
+WHERE winner = 'Albert Einstein' 
+
+SELECT winner
+FROM nobel
+WHERE yr >= 2000 AND subject = 'Peace'
+
+SELECT yr,subject,winner
+FROM nobel
+WHERE yr BETWEEN 1980 AND 1989 
+AND subject = 'Literature'
+
+SELECT * 
+FROM nobel
+WHERE winner IN ('Theodore Roosevelt','Woodrow Wilson','Barack Obama','Jimmy Carter')
+
+SELECT winner
+FROM nobel
+WHERE winner like 'John%'
+
+SELECT yr,subject,winner
+FROM nobel
+WHERE subject = 'Physics' AND yr = '1980'
+OR ​subject = 'Chemistry' AND yr = '1984'
+
+SELECT yr,subject,winner 
+FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('chemistry','medicine')
+
+SELECT yr,subject,winner
+FROM nobel
+WHERE yr < 1910 AND subject = 'medicine'
+OR
+yr >= 2004 AND subject = 'literature'
+
+SELECT *
+FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
+
+SELECT *
+FROM nobel
+WHERE winner LIKE'EUGENE O''NEILL'
+
+SELECT winner,yr,subject
+FROM nobel
+WHERE winner LIKE 'Sir%' ORDER BY yr DESC,winner
+
+-- SELECT IN SELECT
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+SELECT name
+FROM world
+WHERE continent = 'Europe' AND gdp/population >
+(SELECT GDP/POPULATION FROM world 
+WHERE name = 'United Kingdom')
+
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent FROM world WHERE name IN ('Argentina','Australia'))
+ORDER BY name
+
+SELECT name,population
+FROM world
+WHERE population > (SELECT population FROM world WHERE name = 'Canada')
+AND population < (SELECT population FROM world WHERE name = 'Poland')
